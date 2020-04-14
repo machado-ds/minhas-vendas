eb8a7cd147e33a96eeff8ffbead77ef5eeca8489
Breno <breno.mds.2512@gmail.com>
2020-04-14 10:20:06 -0300

    Commit inicial: implementação da Activity de formulário de criação de nova venda e da Activity de carrinho contendo as vendas de um cliente único

---
+++ app/src/main/java/br/com/caranovabrecho/minhasvendas/ui/activity/NovaVendaCarrinhoActivity.java
@@ -0,0 +1,16 @@
+package br.com.caranovabrecho.minhasvendas.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.caranovabrecho.minhasvendas.R;
+
+public class NovaVendaCarrinhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_venda_carrinho);
+    }
+}
