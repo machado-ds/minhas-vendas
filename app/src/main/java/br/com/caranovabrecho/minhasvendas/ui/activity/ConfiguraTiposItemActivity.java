7800497e4c1af3eb1c862167c366727a34f6e6c6
Breno <breno.mds.2512@gmail.com>
2020-05-20 13:37:47 -0300

    Implementação do Adapter do View Pager do fragment de configuração

--- app/src/main/java/br/com/caranovabrecho/minhasvendas/ui/activity/ConfiguraTiposItemActivity.java
+++ app/src/main/java/br/com/caranovabrecho/minhasvendas/ui/activity/ConfiguraTiposItemActivity.java
@@ -1,16 +1,53 @@
 package br.com.caranovabrecho.minhasvendas.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.caranovabrecho.minhasvendas.R;
 
 public class ConfiguraTiposItemActivity extends AppCompatActivity {
 
+    private ViewPager2 viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configura_tipos_item);
+        viewPager = findViewById(R.id.activity_configura_tipo_item_viewpager);
+    }
+
+    private class ConfiguraTipoItemPagerAdapter extends FragmentStateAdapter {
+
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public ConfiguraTipoItemPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+             fragmentList.add(fragment);
+             fragmentTitleList.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragmentList.size();
+        }
     }
 }
--- app/src/main/java/br/com/caranovabrecho/minhasvendas/ui/fragment/IntroParaConfigFragment.java
+++ app/src/main/java/br/com/caranovabrecho/minhasvendas/ui/fragment/IntroParaConfigFragment.java
@@ -2,11 +2,13 @@ package br.com.caranovabrecho.minhasvendas.ui.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.caranovabrecho.minhasvendas.R;
 
@@ -15,15 +17,24 @@ import br.com.caranovabrecho.minhasvendas.R;
  */
 public class IntroParaConfigFragment extends Fragment {
 
+    private Button botaoContinuar;
+
     public IntroParaConfigFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        botaoContinuar = getView().findViewById(R.id.fragment_intro_continuar_button);
+
+        botaoContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //navigate to fragment method
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_intro_para_config, container, false);
     }
 }
